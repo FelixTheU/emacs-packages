5b863fe0d25649114b5fa632c05237db110ad87d
zhengsw <zhengsw@centos7.zhengsw.309>
2022-06-12 11:57:36 +0800

    郑思文: 准备 lite 分支，仅留下 doxymacs 与 spacemacs-theme.

--- cmake-mode-20220322.1258/cmake-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from cmake-mode.el  -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20220322.1258" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "a6487b18cc995b012a5ad3e9056bf5a0ed60d0b4")
